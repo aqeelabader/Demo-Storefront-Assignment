a9d54c709713b420e9b45df3e5b15dc6e356542a
Aqeel Abader <aqeel.abader.aa@outlook.com>
2021-06-12 20:11:13 +0200

    Add project files.

---
+++ prog7311poeTask2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="prog7311poeTask2.MvcApplication" Language="C#" %>
---
+++ prog7311poeTask2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prog7311poeTask2.Startup))]
+namespace prog7311poeTask2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
