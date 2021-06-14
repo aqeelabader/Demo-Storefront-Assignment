dbbb0c1d31a45c357450825ef00ea141eae0ff42
Aqeel Abader <aqeel.abader.aa@outlook.com>
2021-06-14 23:02:28 +0200

    finished

--- prog7311poeTask2/Controllers/ProductController.cs
+++ prog7311poeTask2/Controllers/ProductController.cs
@@ -13,23 +13,37 @@ namespace prog7311poeTask2.Controllers
 {
     public class ProductController : Controller
     {
-        private SqlConnection conn;
+        //code attribution
+        //the method used for generation of views was taken from the following website
+        //https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-a-view
+        //this method is used for creating all the views for this application 
+        //end of code attribution
+
+
+        private SqlConnection conn; //this is just to create the sql connection
         private void Connection()
         {
             string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
             conn = new SqlConnection(constr);
         }
 
-        //get product/getallproducts
-
-
+        // this gets all the products in the database
         public ActionResult GetAllProducts(string prodSearch)
         {
 
-                Connection();
+                Connection();//connection to db
                 List<ProductModel> ProductList = new List<ProductModel>();//creating a list to populate from the databast
 
+                //code attribution
+                //the following code for the search function was taken from a YouTube Video
+                //link to video: https://www.youtube.com/watch?v=UAZABAJlO8g
+                //
+
                 string query = "select * from [dbo].[Product] where ProductName like '%" + prodSearch + "%'";
+
+                //End of attributed code
+
+
                 SqlCommand GetCommand = new SqlCommand(query, conn);
                 //GetCommand.CommandType = CommandType.StoredProcedure;//using the stored procedure from the database
                 SqlDataAdapter da = new SqlDataAdapter(GetCommand);
@@ -56,51 +70,56 @@ namespace prog7311poeTask2.Controllers
                 }
             conn.Close();
             ModelState.Clear();
-            return View(ProductList);
+            return View(ProductList);//returns the list of products
         }
 
         //Get all Hardware products
+        //same as get all products but filtered to hardware
         public ActionResult GetAllHardwareProducts()
         {
-            ProductRepository ProductRepo = new ProductRepository();
+            ProductRepository ProductRepo = new ProductRepository();//uses the repository to avoid repetitive code
             ModelState.Clear();
             return View(ProductRepo.GetAllHardwareProducts());
         }
 
         //Get all Software products
+        //same as get all products but filtered to software
         public ActionResult GetAllSoftwareProducts()
         {
-            ProductRepository ProductRepo = new ProductRepository();
+            ProductRepository ProductRepo = new ProductRepository();//using the repository again
             ModelState.Clear();
             return View(ProductRepo.GetAllSoftwareProducts());
         }
 
         //Get all Other products
+        //same as get all products but filtered to other and accessories
         public ActionResult GetAllOtherProducts()
         {
-            ProductRepository ProductRepo = new ProductRepository();
+            ProductRepository ProductRepo = new ProductRepository();//uses repository again
             ModelState.Clear();
             return View(ProductRepo.GetAllOtherProducts());
         }
 
 
-        //get product/returnproductview
+        //for admins to add products
+        [Authorize (Roles ="Admin")]
         public ActionResult AddProduct()
         {
             return View();
         }
-        //post product/addproduct
+
+        [Authorize(Roles = "Admin")]
         [HttpPost]
-        public ActionResult AddProduct(ProductModel obj)
+        public ActionResult AddProduct(ProductModel obj)//uses the model to get correct format
         {
-            try
+            try//put it in a try so program does not crash if database is not connected 
             {
                 if (ModelState.IsValid)
                 {
-                    ProductRepository ProductRepo = new ProductRepository();
+                    ProductRepository ProductRepo = new ProductRepository();//code for this is in the repository
                     if (ProductRepo.AddProduct(obj))
                     {
-                        ViewBag.Message = "Product added successfully";
+                        ViewBag.Message = "Product added successfully";//just a success message but it serves as confirmation 
 
                     }
                 }
@@ -111,23 +130,29 @@ namespace prog7311poeTask2.Controllers
                 return View();
             }
         }
-        //get product/returneditproductview
-        public ActionResult EditProduct(int id)
+
+
+
+        //cart....when a person clicks add to cart , this handles it
+        [Authorize]
+        public ActionResult Cart(int id)//uses the id to find the product in the all products list
         {
-            ProductRepository ProductRepo = new ProductRepository();
-            return View(ProductRepo.GetAllProducts().Find(Pat => Pat.PId == id));
+            ProductRepository ProductRepo = new ProductRepository();// same repository from get all products, it holds the data needed
+            return View(ProductRepo.GetAllProducts().Find(Pat => Pat.PId == id));//gets the product with the mathcing id
 
         }
-        //post product/editexistingproduct
+        
+        //this is just so the cart can redirect to the payment page
+        [Authorize]
         [HttpPost]
-        public ActionResult EditProduct(int id, ProductModel obj)
+        public ActionResult Cart(int id, ProductModel obj)
         {
             try
             {
                 ProductRepository ProductRepo = new ProductRepository();
                 ProductRepo.UpdateProduct(obj);
 
-                return RedirectToAction("GetAllProducts");
+                return RedirectToAction("Payment");
 
             }
             catch
@@ -136,7 +161,36 @@ namespace prog7311poeTask2.Controllers
 
             }
         }
-        //get product/deleteproduct
+
+
+        //for editing products in the database
+        public ActionResult EditProduct(int id)//uses the id of selected product
+        {
+            ProductRepository ProductRepo = new ProductRepository();//finds it using the repository
+            return View(ProductRepo.GetAllProducts().Find(Pat => Pat.PId == id));//compares the id of selected product to locate it in the repo
+
+        }
+        //updates the database
+        [HttpPost]
+        public ActionResult EditProduct(int id, ProductModel obj)//uses the model again for formatting
+        {
+            try
+            {
+                ProductRepository ProductRepo = new ProductRepository();//referencing the repo cause thats where the code is
+                ProductRepo.UpdateProduct(obj);//updates the db using the new inputs
+
+                return RedirectToAction("GetAllProducts");//redirects back to the all products page when done
+
+            }
+            catch
+            {
+                return View();
+
+            }
+        }
+
+        //code to delete a product
+        //nothing special here, just deletes the product that matches id of selected product in database.
         public ActionResult DeleteProduct(int id)
         {
             try
@@ -153,6 +207,22 @@ namespace prog7311poeTask2.Controllers
             {
                 return View();
             }
+        }
+
+        //this is just to create the success page that is shown after payment is made
+        public ActionResult Success()
+        {
+            ViewBag.Message = "the success page";
+
+            return View();//returns the success view
+        }
+
+        //this is just to create the payment page where a user enters their payment info
+        public ActionResult Payment()
+        {
+            ViewBag.Message = "the payment page";
+
+            return View();//just returns the view.....POE document said assume payment was successfull so no code done here!
         }
     }
 }
--- prog7311poeTask2/Repository/ProductRepository.cs
+++ prog7311poeTask2/Repository/ProductRepository.cs
@@ -7,22 +7,21 @@ using System.Data;
 using System.IO;
 using System.Configuration;
 using prog7311poeTask2.Models;
-
+using System.Text;
 
 namespace prog7311poeTask2.Repository
 {
     public class ProductRepository
     {
-        private SqlConnection conn;
+        private SqlConnection conn;//makes the sql connection variable
 
-        private void Connection()
+        private void Connection()//class for the sql connection so it can be used where needed
         {
-            string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+            string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();//the database used is the default db generated for login
             conn = new SqlConnection(constr);
         }
 
         //get all products in list
-
         public List<ProductModel> GetAllProducts()
         {
             Connection();
@@ -54,17 +53,18 @@ namespace prog7311poeTask2.Repository
             return ProductList;
         }
 
+        //same as get all products but for hardware
         public List<ProductModel> GetAllHardwareProducts()
         {
             Connection();
             List<ProductModel> ProductList = new List<ProductModel>();//creating a list to populate from the databast
 
-            SqlCommand GetCommand = new SqlCommand("GetAllHardwareProducts", conn);
+            SqlCommand GetCommand = new SqlCommand("GetAllHardwareProducts", conn);//pulls hardware products from the database
             GetCommand.CommandType = CommandType.StoredProcedure;//using the stored procedure from the database
             SqlDataAdapter da = new SqlDataAdapter(GetCommand);
-            DataTable dt = new DataTable();
+            DataTable dt = new DataTable();//the data will be added to this table
 
-            conn.Open();
+            conn.Open();//opening connection and executing , then closing connection
             da.Fill(dt);
             conn.Close();
             //list is binded using a data row
@@ -82,9 +82,9 @@ namespace prog7311poeTask2.Repository
                     }
                     );
             }
-            return ProductList;
+            return ProductList;//returns the list that matches hardware parameter
         }
-
+        //same as above but for software products only 
         public List<ProductModel> GetAllSoftwareProducts()
         {
             Connection();
@@ -113,9 +113,10 @@ namespace prog7311poeTask2.Repository
                     }
                     );
             }
-            return ProductList;
+            return ProductList;//returns all software products
         }
 
+        //same as get all hardware but for the other products
         public List<ProductModel> GetAllOtherProducts()
         {
             Connection();
@@ -144,36 +145,47 @@ namespace prog7311poeTask2.Repository
                     }
                     );
             }
-            return ProductList;
+            return ProductList;//returns all other products
         }
 
         //adding new products
-
-        public bool AddProduct(ProductModel obj)
+        //this is the repository code for adding a product to the database
+        public bool AddProduct(ProductModel obj)//uses product model 
         {
-            string FileName = Path.GetFileNameWithoutExtension(obj.ImageFile.FileName) ;
+            //code attribution
+            //the process below of getting the file path was taken from Stack Overflow
+            ///original question: https://stackoverflow.com/questions/1268738/asp-net-mvc-find-absolute-path-to-the-app-data-folder-from-controller
+            //answer user details:
+            //Alex from Jitbit
+            //https://stackoverflow.com/users/56621/alex-from-jitbit
+            //
+
+            string FileName = Path.GetFileNameWithoutExtension(obj.ImageFile.FileName) ;//this is just getting the path for the product picture
             string FileExtension = Path.GetExtension(obj.ImageFile.FileName);
 
-            string relativePath = ConfigurationManager.AppSettings["ImagePath"].ToString();
+            string relativePath = ConfigurationManager.AppSettings["ImagePath"].ToString();//converts the path to a string
 
-            FileName = FileName.Trim() + FileExtension;
+            FileName = FileName.Trim() + FileExtension;//gets extension
 
-            obj.ProductPic = relativePath + FileName;
+            obj.ProductPic = relativePath + FileName;//gets the path of the project and the image in the project....this is what is actually saved in the db
 
+            //end of attributed code
 
             Connection();
-            SqlCommand AddCommand = new SqlCommand("AddNewProduct", conn);
-            AddCommand.CommandType = CommandType.StoredProcedure;
+            SqlCommand AddCommand = new SqlCommand("AddNewProduct", conn);//sql connection
+            AddCommand.CommandType = CommandType.StoredProcedure;//specifying command type
+
+            //adding values
             AddCommand.Parameters.AddWithValue("@ProductName", obj.ProductName);
             AddCommand.Parameters.AddWithValue("@ProductDescription", obj.ProductDescription);
             AddCommand.Parameters.AddWithValue("@ProductCategory", obj.ProductCategory);
             AddCommand.Parameters.AddWithValue("@ProductPrice", obj.ProductPrice);
             AddCommand.Parameters.AddWithValue("@ProductPic", obj.ProductPic);
 
-            conn.Open();
-            int i = AddCommand.ExecuteNonQuery();
-            conn.Close();
-            if(i >= 1)
+            conn.Open();//open connection to db
+            int i = AddCommand.ExecuteNonQuery();//executing command 
+            conn.Close();//close connection to db
+            if(i >= 1)//checking if it was successfully added
             {
                 return true;
             }
@@ -182,15 +194,14 @@ namespace prog7311poeTask2.Repository
                 return false;
             }
         }
-
-
+        
         //updating an existing product in the database
-
-        public bool UpdateProduct(ProductModel obj)
+        public bool UpdateProduct(ProductModel obj)//this is the edit product code
         {
             Connection();
-            SqlCommand UpdateCommand = new SqlCommand("UpdateProduct", conn);
+            SqlCommand UpdateCommand = new SqlCommand("UpdateProduct", conn);//db connection and command
 
+            //these values update the product that was searched for using id in the controller
             UpdateCommand.CommandType = CommandType.StoredProcedure;
             UpdateCommand.Parameters.AddWithValue("@PId", obj.PId);
             UpdateCommand.Parameters.AddWithValue("@ProductName", obj.ProductName);
@@ -199,9 +210,9 @@ namespace prog7311poeTask2.Repository
             UpdateCommand.Parameters.AddWithValue("@ProductPrice", obj.ProductPrice);
             UpdateCommand.Parameters.AddWithValue("@ProductPic", obj.ProductPic);
 
-            conn.Open();
-            int i = UpdateCommand.ExecuteNonQuery();
-            conn.Close();
+            conn.Open();//open db connection
+            int i = UpdateCommand.ExecuteNonQuery();//execute
+            conn.Close();//close db connection
             if (i >= 1)
             {
                 return true;
@@ -210,22 +221,16 @@ namespace prog7311poeTask2.Repository
             {
                 return false;
             }
-
-
         }
-
-
-
-
         // deleting a product
-
         public bool DeleteProduct(int ProductId)
         {
             Connection();
-            SqlCommand DeleteCommand = new SqlCommand("DeleteProductById", conn);
-            DeleteCommand.CommandType = CommandType.StoredProcedure;
+            SqlCommand DeleteCommand = new SqlCommand("DeleteProductById", conn);//gets the product by id
+            DeleteCommand.CommandType = CommandType.StoredProcedure;//specifies stored procedure
             DeleteCommand.Parameters.AddWithValue("@PId", ProductId);
 
+            //db open , execute and close
             conn.Open();
             int i = DeleteCommand.ExecuteNonQuery();
             conn.Close();
@@ -237,12 +242,6 @@ namespace prog7311poeTask2.Repository
             {
                 return false;
             }
-
         }
-         
-
-
-
-
     }
 }
